c805a04451c3c32871a235f178ead87c1aa113a0
pradeep-sirpi <pradeep@sirpi.io>
2024-01-11 12:13:38 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/home/Home';
+import Canvas from './pages/canvas/Canvas';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/canvas" element={<Canvas />} />
+    </Routes>
+  );
+}
+
+export default App;

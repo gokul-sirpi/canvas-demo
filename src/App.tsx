d7d6fd970ba8c9acd0e703ce07558b67dfef884f
pradeep-sirpi <pradeep@sirpi.io>
2024-02-12 13:09:39 +0530

    removed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,93 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Canvas from './pages/canvas/Canvas';
+import { useEffect, useRef, useState } from 'react';
+import keycloak from './lib/keycloak';
+import envurls from './utils/config';
+import { axiosAuthClient } from './lib/axiosConfig';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/canvas" element={<Canvas />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const isRun = useRef(false);
+  const windowref = useRef<Window | null>(null);
+  const intervalId = useRef<number>();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (isRun.current) return;
+    isRun.current = true;
+    initialiseKeycloak();
+  }, []);
+
+  function initialiseKeycloak() {
+    keycloak
+      .init({
+        onLoad: 'check-sso',
+      })
+      .then((authenticated: boolean) => {
+        if (authenticated) {
+          checkUserProfile();
+        } else {
+          setCookie('gsx-ui-sso', '');
+          handleLogin();
+        }
+      })
+      .catch((err: Error) => {
+        console.log(err);
+      });
+  }
+
+  function setCookie(key: string, val: string) {
+    document.cookie = `${key}=${val};domain=iudx.io`;
+  }
+
+  function handleLogin() {
+    windowref.current = window.open(envurls.authReduirectUrl, '_blank');
+    intervalId.current = setInterval(() => {
+      checkLoginStatus();
+    }, 500);
+  }
+
+  function getCookieValue(cname: string) {
+    const cookies = document.cookie.split(';');
+    let returnVal;
+    for (const cookie of cookies) {
+      if (!cookie) continue;
+      const cookieKey = cookie.split('=')[0].trim();
+      const cookieValue = cookie.split('=')[1].trim();
+      if (cookieKey === cname) {
+        returnVal = cookieValue;
+      }
+    }
+    return returnVal;
+  }
+
+  function checkLoginStatus() {
+    const cookieResponse = getCookieValue('gsx-ui-sso');
+    if (cookieResponse === 'logged-in') {
+      clearInterval(intervalId.current);
+      closeAuthTab();
+      window.location.reload();
+    }
+  }
+
+  function closeAuthTab() {
+    if (windowref.current) {
+      windowref.current.close();
+    }
+  }
+
+  async function checkUserProfile() {
+    try {
+      const response = await axiosAuthClient.get('v1/user/profile');
+      if (response.status === 200 && response.data.results) {
+        if (response.data.results.roles.length > 0) {
+          setLoggedIn(true);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return <>{loggedIn ? <Home /> : <Canvas />}</>;
 }
 
 export default App;
